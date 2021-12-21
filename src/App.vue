0872dcf5227aac53d7c35b4bc2fdc1f70728277e
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-12-21 19:10:30 +0100

    da completare

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <Main />
+    <main>
+      <Main />
+    </main>
     <prefooter />
     <footer-banner />
     <Footer />
---
+++ src/components/comicsItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="single-card">
+        <img :src="comicObject.thumb" :alt="comicObject.series">
+        <h4>{{comicObject.series}}</h4>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name: "comicsItem",
+    props: []
+    
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables';
+
+    
+.single-card{
+    width: calc((100% / 6) - 10px); 
+    margin: 4px;
+    img{
+        width: 100%;
+        height: auto;
+    }
+}
+
+
+
+</style>
